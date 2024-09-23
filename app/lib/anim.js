2112177682c53d06b9a264026fc44cb4a3d16137
midCenturyDude7 <mgriffes@gmail.com>
2024-09-22 19:57:24 -0700

    Fixed bug in slide and scale

--- app/lib/anim.js
+++ app/lib/anim.js
@@ -11,11 +11,11 @@ export const slide = {
   initial: { x: 80 },
   enter: (i) => ({
     x: 0,
-    transition: { duration: 0.8, ease: [0.76, 0, 0.24, 1], delay: 0.5 * i },
+    transition: { duration: 0.8, ease: [0.76, 0, 0.24, 1], delay: 0.05 * i },
   }),
   exit: (i) => ({
     x: 80,
-    transition: { duration: 0.8, ease: [0.76, 0, 0.24, 1], delay: 0.5 * i },
+    transition: { duration: 0.8, ease: [0.76, 0, 0.24, 1], delay: 0.05 * i },
   }),
 };
 
